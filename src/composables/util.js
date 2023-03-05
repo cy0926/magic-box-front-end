5eeb03794a3aa2b62a2003d3ddda1ec1e55ac14e
yukin <709695726@qq.com>
2023-03-05 12:08:52 +0800

    🌟feat(web): 新增退出登录功能

--- src/composables/util.js
+++ src/composables/util.js
@@ -1,10 +1,19 @@
 import { ElNotification } from 'element-plus'
-
+import { ElMessageBox } from 'element-plus'
 // 消息提示
 export function toast(message, type = 'success') {
   ElNotification({
     message: message,
     type,
     duration: 1000
+  })
+}
+
+//弹出提示框
+export function showModel(content = '提示内容', type = 'warning', title = '') {
+  return ElMessageBox.confirm(content, title, {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type
   })
 }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,32 @@
 <template>
   <div>首页</div>
-  <button @click="addCount()">{{ count }}</button>
+  <el-button @click="handleLogout">退出登录</el-button>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-const count = ref(1)
+import { useRouter } from 'vue-router'
+import { showModel, toast } from '@/composables/util'
+// import { logout } from '@/api/login'
 
-function addCount(params) {
-  console.log('addCount')
-  count.value++
-  console.log(count.value)
+const router = useRouter()
+function handleLogout() {
+  showModel('是否要退出登录？').then((res) => {
+    // 真实的登录接口
+    // logout().finally(() => {
+    // 移除cookies里的token
+    //清楚当前用户状态vuex
+
+    //跳转回首页
+    // router.push('/login')
+    //提示退出登录成功
+    // toast('退出登录成功')
+    // })
+
+    //只做跳转会首页，不掉退出登录接口
+    //跳转回首页
+    router.push('/login')
+    //提示退出登录成功
+    toast('退出登录成功')
+  })
 }
 </script>
